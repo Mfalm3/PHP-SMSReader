3e78402ad5f1e7d5afe47c481c3e60c30613ee26
Mfalm3 <waitha28ka@gmail.com>
2016-12-06 14:17:06 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>List of available Sms Logs - Shen Inc. Sms Reader</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/filelist.css">
+</head>
+
+<body>
+<div id="wrapper">
+<br><br><br><br><br>
+<table width="76%" border="0" align="center" cellspacing="0" cellpadding="3" class="tableWidth">
+	<tr>
+	<td id="subTitle">Shen Inc. Sms Reader - Open Source sms reader</td>
+	</tr>
+</table>
+<table width="76%" border="0" align="center" cellspacing="0" cellpadding="0" class="tableWidth">
+<tr class="blak">
+<td>
+	<table width="100%" border="0" align="center" cellspacing="1" cellpadding="0">
+	<tr>
+	<td colspan="6"> 
+		<table width="100%" border="0" align="center" cellspacing="0" cellpadding="10">
+		<tr> 
+		<td id="pageContent"> 
+
+
+<h1 align=Center>Index of locally available logs:</h1>
+  <table border="0" width="100%" cellspacing="1" cellpadding="0">
+		<th>
+		<br/>
+			No categories
+		</th>
+		<?php
+		include_once('Utils.php');
+		list_files();
+		?> 
+	</table>
+	<br>
+		</td>
+		</tr>
+		</table>
+	</td>
+	</tr>
+	</table>
+</td>
+</tr>
+</table>
+
+<table width="76%" border="0" align="center" valign="bottom" cellspacing="0" cellpadding="0">
+	<tr>
+	<td id="footer" align="right"><small>&copy; 2016 Shen Inc.</small></td>
+	</tr>
+</table>
+</div>
+</div>
+</body>
+</html>
